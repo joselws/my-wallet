544f80c9c703746600723098b00969a589c685bc
Jose Wilhelm <josel.wilhelms@gmail.com>
2022-05-29 01:37:38 -0400

    main gets the remaining percent for 100

--- Account.py
+++ Account.py
@@ -180,9 +180,14 @@ class Account():
 
         print("Showing current wallet percentages")
         for wallet in self.wallets:
-            print(f'Wallet {wallet.name}: {wallet.percent}%')
+            if wallet.name != 'main':
+                print(f'Wallet {wallet.name}: {wallet.percent}%')
 
+        print('')   # newline
         for wallet in self.wallets:
+            if wallet.name == 'main':
+                continue
+
             new_percent = input(f'Set percent of Wallet {wallet.name}: ')
             try:
                 new_percent = int(new_percent)
@@ -194,6 +199,15 @@ class Account():
                     wallet.percent = new_percent
                 else:
                     print('Not a valid number format, please try again.')
+            # Set 'main' wallet percent to be the remaining percent to complete 100
+            finally:
+                percents = sum([wallet.percent for wallet in self.wallets if wallet.name != 'main'])
+                if percents < 100:
+                    main = self.get_wallet('main')
+                    main.percent = 100 - percents
+                    print('Percents correctly set.\n')
+                else:
+                    print('Percents not correctly set, please try again.\n')
     
     def deposit(self, amount: int) -> None:
         """
