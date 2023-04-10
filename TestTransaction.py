2ddf4dac1af9cc32dc3fd0d0dfe95f74f22018f2
Jose Wilhelm <josel.wilhelms@gmail.com>
2023-04-10 04:47:34 -0400

    TransactionHistory class advances

--- TestTransaction.py
+++ TestTransaction.py
@@ -23,6 +23,24 @@ class TestTransaction(unittest.TestCase):
         self.assertEqual(transaction.balance_before, 500)
         self.assertEqual(transaction.balance_after, 300)
 
+    def test_transaction_correctly_created_with_other_datatypes(self):
+        transaction = Transaction(
+            date=datetime(1995, 6, 12, 0, 0, 0),
+            wallet_name="main",
+            transaction_type="deduction",
+            amount="200",
+            description="test transaction",
+            balance_before="500",
+            balance_after="300"
+        )
+        self.assertEqual(transaction.date, datetime(1995, 6, 12, 0, 0, 0))
+        self.assertEqual(transaction.wallet_name, "main")
+        self.assertEqual(transaction.transaction_type, "deduction")
+        self.assertEqual(transaction.amount, 200)
+        self.assertEqual(transaction.description, "test transaction")
+        self.assertEqual(transaction.balance_before, 500)
+        self.assertEqual(transaction.balance_after, 300)
+
 
 if __name__ == "__main__":
     unittest.main()
--- Transaction.py
+++ Transaction.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
+import csv
 
 @dataclass(frozen=True)
 class Transaction:
     date: datetime
-    wallet_name: str
+    wallet: str
     transaction_type: str
     amount: int
     description: str
@@ -13,4 +14,61 @@ class Transaction:
 
 
 class TransactionHistory:
-    pass
+    
+    def __init__(self):
+        self.transactions = []
+        self.transactions_filename = "transactions.csv"
+        self.date_format = "%d-%m-%Y %H:%M:%S"
+        self.headers = [
+            "date",
+            "wallet",
+            "transaction_type",
+            "amount",
+            "description",
+            "balance_before",
+            "balance_after"
+        ]
+
+    def load_transactions(self) -> int:
+        """
+        Read the contents of the transactions file,
+        parses the columns of each member,
+        creates a transaction object for each entry,
+        and appends it to self.transactions list attribute
+        """
+
+        try:
+            self.transactions.clear()
+            with open(self.transactions_filename, "r", newline="") as csvfile:
+                transaction_entries = csv.DictReader(csvfile)
+                for row in transaction_entries:
+                    if not (transaction := self._convert_into_object(row)):
+                        return 2
+                    self.transactions.append(transaction)
+        except FileNotFoundError:
+            print(f"File {self.transactions_filename} not found")
+            return 1
+        else:
+            print("Transactions data loaded")
+            return 0
+        
+    def _convert_into_object(self, transaction_entry: dict) -> Transaction:
+        """
+        
+        """
+
+        try:
+            transaction = Transaction(
+                date=datetime.strptime(transaction_entry["date"], self.date_format),
+                wallet=transaction_entry["wallet"],
+                transaction_type=transaction_entry["transaction_type"],
+                amount=int(transaction_entry["amount"]),
+                description=transaction_entry["description"],
+                balance_before=int(transaction_entry["balance_before"]),
+                balance_after=int(transaction_entry["balance_after"])
+            )
+        except ValueError as error:
+            print(f"Error {error} with a transaction atribute type {transaction_entry}")
+            return None
+        else:
+            return transaction
