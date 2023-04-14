27552505fa30d9a2d14037c52cfb97500a93d68a
Jose Wilhelm <josel.wilhelms@gmail.com>
2023-04-14 00:36:20 -0400

    Added TransferTransaction class and TransactionType enum

--- TestTransaction.py
+++ TestTransaction.py
@@ -1,45 +1,56 @@
 import unittest
-from Transaction import Transaction
+from Transaction import DeductTransaction, TransferTransaction, TransactionType
 from datetime import datetime
 
 
-class TestTransaction(unittest.TestCase):
+class TestDeductTransaction(unittest.TestCase):
 
-    def test_transaction_correctly_created(self):
-        transaction = Transaction(
+    def test_deduct_transaction_correctly_created(self):
+        transaction = DeductTransaction(
             date=datetime(1995, 6, 12, 0, 0, 0),
-            wallet_name="main",
-            transaction_type="deduction",
+            wallet="main",
+            transaction_type=TransactionType.DEDUCTION.value,
             amount=200,
-            description="test transaction",
+            description="test deduct transaction",
             balance_before=500,
             balance_after=300
         )
         self.assertEqual(transaction.date, datetime(1995, 6, 12, 0, 0, 0))
-        self.assertEqual(transaction.wallet_name, "main")
+        self.assertEqual(transaction.wallet, "main")
         self.assertEqual(transaction.transaction_type, "deduction")
         self.assertEqual(transaction.amount, 200)
-        self.assertEqual(transaction.description, "test transaction")
+        self.assertEqual(transaction.description, "test deduct transaction")
         self.assertEqual(transaction.balance_before, 500)
         self.assertEqual(transaction.balance_after, 300)
 
-    def test_transaction_correctly_created_with_other_datatypes(self):
-        transaction = Transaction(
+
+class TestTransferTransaction(unittest.TestCase):
+
+    def test_transfer_transaction_correctly_created(self):
+        transaction = TransferTransaction(
             date=datetime(1995, 6, 12, 0, 0, 0),
-            wallet_name="main",
-            transaction_type="deduction",
-            amount="200",
-            description="test transaction",
-            balance_before="500",
-            balance_after="300"
+            from_wallet_name="main",
+            to_wallet_name="charity",
+            transaction_type=TransactionType.TRANSFER.value,
+            amount=200,
+            description="test transfer transaction",
+            from_wallet_balance_before=500,
+            from_wallet_balance_after=300,
+            to_wallet_balance_before=1000,
+            to_wallet_balance_after=1200,
         )
         self.assertEqual(transaction.date, datetime(1995, 6, 12, 0, 0, 0))
-        self.assertEqual(transaction.wallet_name, "main")
-        self.assertEqual(transaction.transaction_type, "deduction")
+        self.assertEqual(transaction.from_wallet_name, "main")
+        self.assertEqual(transaction.to_wallet_name, "charity")
+        self.assertEqual(transaction.transaction_type, "transfer")
         self.assertEqual(transaction.amount, 200)
-        self.assertEqual(transaction.description, "test transaction")
-        self.assertEqual(transaction.balance_before, 500)
-        self.assertEqual(transaction.balance_after, 300)
+        self.assertEqual(transaction.description, "test transfer transaction")
+        self.assertEqual(transaction.from_wallet_balance_before, 500)
+        self.assertEqual(transaction.from_wallet_balance_after, 300)
+        self.assertEqual(transaction.to_wallet_balance_before, 1000)
+        self.assertEqual(transaction.to_wallet_balance_after, 1200)
+
+    
 
 
 if __name__ == "__main__":
--- Transaction.py
+++ Transaction.py
@@ -1,13 +1,33 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
+
+
+class TransactionType(Enum):
+    DEDUCTION = "deduction"
+    TRANSFER = "transfer"
 
 
 @dataclass(frozen=True)
-class Transaction:
+class DeductTransaction:
     date: datetime
     wallet: str
-    transaction_type: str
+    transaction_type: TransactionType.DEDUCTION
     amount: int
     description: str
     balance_before: int
     balance_after: int
+
+
+@dataclass(frozen=True)
+class TransferTransaction:
+    date: datetime
+    transaction_type: TransactionType.TRANSFER
+    from_wallet_name: str
+    to_wallet_name: str
+    amount: int
+    description: str
+    from_wallet_balance_before: int
+    from_wallet_balance_after: int
+    to_wallet_balance_before: int
+    to_wallet_balance_after: int
